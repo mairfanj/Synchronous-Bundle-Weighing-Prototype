72c51b40c379db43c3bdfaa2ea914ec092a0756b
mairfanj <ecoverapple@gmail.com>
2025-01-19 21:55:08 +0800

    Update config.h
    Insert loadcell HX711 data pin

--- Firmware/master_device/config.h
+++ Firmware/master_device/config.h
@@ -12,14 +12,14 @@
 #define LCD_COLUMNS 16       // Number of columns
 #define LCD_ROWS 2           // Number of rows
 
+// ----- Load Cell Settings -----
+#define LOADCELL_DOUT_PIN 16 // HX711 data pin
+#define LOADCELL_SCK_PIN 17  // HX711 clock pin
+
 // ----- Keypad Settings -----
 #define KEYPAD_PIN 4         // Pin connected to the keypad (used for master detection)
 
-// ----- Calibration Settings -----
-#define DEFAULT_COPY_WEIGHT 1.0 // Default weight per copy in grams
-
-// ----- Other Settings -----
+// ----- General Settings -----
 #define SERIAL_BAUD_RATE 115200 // Serial communication baud rate
 
 #endif // CONFIG_H
- 
